962f89eed34df4cc295c7c04e8c51ba139c0a9d3
Lian Liu <45963690+Geo-LianLiu@users.noreply.github.com>
2023-12-15 15:37:58 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-'''
-@File    :   setup.py
-@Time    :   2023/12/08 15:18:46
-@Version :   1.0
-@Author  :   Lian Liu
-@Contact :   lianliu1017@126.com
-@License :   (C)Copyright 2023, Lian Liu
-@Desc    :   
-@Run     :   python setup.py build; python setup.py install
-'''
-
-# 1. Import modules
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# Copyright 2023 Lian Liu. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ====================================================================== #
+#                       @Author  :   Lian Liu                            #
+#                       @e-mail  :   lianliu1017@126.com                 #
+# ====================================================================== #
 
 from setuptools import setup, find_packages
 
